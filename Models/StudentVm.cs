09ce74051ef51f7585923310c28cafc07ed64c8a
Prajwal Prasain <prasainprajwal1987@gmail.com>
2023-05-25 12:22:30 +0545

    Initial Commit

---
+++ Models/StudentVm.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication.Models
+{
+    public class StudentVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }    
+        public string Address { get; set; }
+              
+    }
+}
